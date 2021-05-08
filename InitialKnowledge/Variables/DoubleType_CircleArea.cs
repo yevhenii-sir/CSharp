7352a59f2cb6643c3f7ef2db9de490feaf6d6704
Yevhenii Sirenko <ew_sirenko@ukr.net>
2021-05-08 13:17:50 +0300

    Using double type

---
+++ InitialKnowledge/Variables/DoubleType_CircleArea.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Circle
+{
+    static void Main()
+    {
+        double radius;
+        double area;
+
+        radius = 10.0;
+        area = radius * radius * 3.1416;
+
+        Console.WriteLine("Area circle - " + area);
+    }
+}
