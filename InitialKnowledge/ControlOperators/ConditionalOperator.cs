4d3e9133187fbd511fd1b70edeb400ea97493f40
Yevhenii Sirenko <ew_sirenko@ukr.net>
2021-05-08 13:32:09 +0300

    Using a conditional operator(if, if/else)

---
+++ InitialKnowledge/ControlOperators/ConditionalOperator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class IfDemo
+{
+    static void Main()
+    {
+        int a, b, c;
+
+        a = 2;
+        b = 3;
+
+        if (a < b) Console.WriteLine("а меньше b");
+        if (a == b) Console.WriteLine("Этого никто не увидит!");
+
+        Console.WriteLine();
+
+        c = a - b;
+
+        Console.WriteLine("c содержит -1");
+        if (c >= 0) Console.WriteLine("Значение неотрецательно!");
+        else Console.WriteLine("Значение отрицательно");
+
+        Console.WriteLine();
+
+        c = b - a;
+
+        Console.WriteLine("c содержит 1");
+        if (c >= 0) Console.WriteLine("Значение неотрецательно!");
+        if (c < 0) Console.WriteLine("Значение отрицательно");
+    }
+}
