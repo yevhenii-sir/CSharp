cd5e67745df623125274844f8e2a244055df3e55
Yevhenii Sirenko <ew_sirenko@ukr.net>
2021-05-08 12:19:23 +0300

    use variables

---
+++ InitialKnowledge/Variables/ApplicationOfVariables.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Example
+{
+    static void Main()
+    {
+        int x;
+        int y;
+
+        x = 100;
+
+        Console.WriteLine("x contains " + x);
+
+        y = x / 2;
+
+        Console.Write("y contains x / 2: ");
+        Console.WriteLine(y);
+    }
+}
