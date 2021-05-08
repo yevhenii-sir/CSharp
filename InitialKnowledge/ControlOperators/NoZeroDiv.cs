03ed89b6e43586f33a69c03cb5034a059836b3e5
Yevhenii Sirenko <ew_sirenko@ukr.net>
2021-05-08 18:41:45 +0300

    Add example of using ternary operator

---
+++ InitialKnowledge/ControlOperators/NoZeroDiv.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class NoZeroDiv
+{
+    static void Main()
+    {
+        int result;
+
+        for (int i = -5; i < 6; i++)
+        {
+            result = (i != 0) ? 100 / i : 0;
+            if (i != 0)
+                Console.WriteLine("100 / " + i + " равно " + result);
+        }
+    }
+}
