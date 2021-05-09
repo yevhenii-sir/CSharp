82ca0bb07f2afc3987691dea6d91969e6bdeb14c
Yevhenii Sirenko <ew_sirenko@ukr.net>
2021-05-09 22:24:14 +0300

    Add example of using with parameters

---
+++ Classes/PrimeNumber/ChkNum.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Basic_Learn_Csharp
+{
+    class ChkNum
+    {
+        public bool IsPrime(int x)
+        {
+            if (x <= 1) return false;
+
+            for (int i = 2; i <= x / i; i++)
+            {
+                if ((x % i) == 0) return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ Classes/PrimeNumber/PramDemo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Basic_Learn_Csharp
+{
+    class ParamDemo
+    {
+        static void Main()
+        {
+            ChkNum ob = new ChkNum();
+
+            for (int i = 2; i < 10; i++)
+            {
+                if (ob.IsPrime(i)) Console.WriteLine(i + " - простое число.");
+                else Console.WriteLine(i + " - непростое число.");  
+            }
+        }
+    }
+}
