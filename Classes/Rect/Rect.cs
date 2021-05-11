dde456b7457f40c019af0ebfa41cd57472ba877b
Yevhenii Sirenko <ew_sirenko@ukr.net>
2021-05-11 19:23:00 +0300

    Add example rect

---
+++ Classes/Rect/Rect.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Basic_Learn_Csharp
+{
+    class Rect
+    {
+        public int Width;
+        public int Height;
+
+        public Rect(int w, int h)
+        {
+            Width = w;
+            Height = h;
+        }
+
+        public int Area()
+        {
+            return Width * Height;
+        }
+    }
+}
---
+++ Classes/Rect/UseRect.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Basic_Learn_Csharp
+{
+    class UseRect
+    {
+        static void Main()
+        {
+            Rect r1 = new Rect(4, 5);
+            Rect r2 = new Rect(7, 9);
+
+            Console.WriteLine("Площадь прямоугольника r1:" + r1.Area());
+
+            Console.WriteLine("Площадь прямоугольника r2:" + r2.Area());
+        }
+    }
+}
