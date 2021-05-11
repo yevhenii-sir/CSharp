adf41140e855220c552d9029c9d7b78327c80e2a
Yevhenii Sirenko <ew_sirenko@ukr.net>
2021-05-11 19:54:55 +0300

    Add example average numbers

---
+++ Arrays/Average.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Basic_Learn_Csharp
+{
+    class Average
+    {
+        static void Main()
+        {
+            int[] nums = new int[10];
+            int avg = 0;
+
+            nums[0] = 99;
+            nums[1] = 10;
+            nums[2] = 100;
+            nums[3] = 18;
+            nums[4] = 78;
+            nums[5] = 23;
+            nums[6] = 63;
+            nums[7] = 9;
+            nums[8] = 87;
+            nums[9] = 49;
+
+            for (int i = 0; i < 10; i++)
+            {
+                avg += nums[i];
+            }
+
+            avg = avg / 10;
+
+            Console.WriteLine("Среднее: " + avg);
+        }
+    }
+}
