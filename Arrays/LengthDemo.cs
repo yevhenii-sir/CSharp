4520322ef869fcc2807ab2babaeb999fdb842e27
Yevhenii Sirenko <ew_sirenko@ukr.net>
2021-05-12 23:46:42 +0300

    Learn length array function

---
+++ Arrays/LengthDemo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Basic_Learn_Csharp
+{
+    class LengthDemo
+    {
+        static void Main()
+        {
+            int[] nums = new int[10];
+
+            Console.WriteLine("Длина масива nums равна " + nums.Length);
+            for (int i = 0; i < nums.Length; i++)
+            {
+                nums[i] = i * i;
+            }
+
+            Console.Write("Содержимое масива nums: ");
+            for (int i = 0; i < nums.Length; i++)
+            {
+                Console.Write(nums[i] + " ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
