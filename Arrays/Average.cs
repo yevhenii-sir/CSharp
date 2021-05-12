d168312f6fdb1d1b7923c6fa246d5de2084f4be9
Yevhenii Sirenko <ew_sirenko@ukr.net>
2021-05-12 23:12:54 +0300

    Added array initialization

--- Arrays/Average.cs
+++ Arrays/Average.cs
@@ -6,19 +6,8 @@ namespace Basic_Learn_Csharp
     {
         static void Main()
         {
-            int[] nums = new int[10];
+            int[] nums = { 99, 10, 100, 18, 78, 23, 63, 9, 87, 49 };
             int avg = 0;
-
-            nums[0] = 99;
-            nums[1] = 10;
-            nums[2] = 100;
-            nums[3] = 18;
-            nums[4] = 78;
-            nums[5] = 23;
-            nums[6] = 63;
-            nums[7] = 9;
-            nums[8] = 87;
-            nums[9] = 49;
 
             for (int i = 0; i < 10; i++)
             {
