f32907928bacbab8dc85763ccf5d0f09994cacc7
Yevhenii Sirenko <ew_sirenko@ukr.net>
2021-05-12 23:22:32 +0300

    Learn two-dimensional array

---
+++ Arrays/TwoD.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Basic_Learn_Csharp
+{
+    class TwoD
+    {
+        static void Main()
+        {
+            int t, i;
+            int[,] table = new int[3, 4];
+
+            for (t = 0; t < 3; ++t)
+            {
+                for (i = 0; i < 4; ++i)
+                {
+                    table[t, i] = (t * 4) + i + 1;
+                    Console.Write(table[t,i] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
