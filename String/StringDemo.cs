3df79416453ca6ea1382272839fa456d84fdaf94
Yevhenii Sirenko <ew_sirenko@ukr.net>
2021-05-15 23:05:22 +0300

    Learn string type

---
+++ String/StringDemo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace BasicLearnCSharp
+{
+    class StringDemo
+    {
+        static void Main(string[] args)
+        {
+            char[] charray = {'Э', 'т', 'о', ' ', 'с', 'т', 'р', 'о', 'к', 'а'};
+
+            string str1 = new string(charray);
+            string str2 = "Еще одна строка.";
+            
+            Console.WriteLine(str1);
+            Console.WriteLine(str2);
+        }
+    }
+}
