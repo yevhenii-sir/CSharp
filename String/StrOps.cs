e8ee366974bb8fd60455a72d0973450e84304352
Yevhenii Sirenko <ew_sirenko@ukr.net>
2021-05-15 23:41:11 +0300

    Add an example of handling strings

---
+++ String/StrOps.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Globalization;
+
+namespace BasicLearnCSharp
+{
+    class StrOps
+    {
+        static void Main(string[] args)
+        {
+            string str1 = "Програмировать в .NET лутше всего на С#.";
+            string str2 = "Програмировать в .NET лутше всего на С#.";
+            string str3 = "Строки в С# весьма эффективны.";
+            string strUp, strLow;
+            int result, idx;
+            
+            Console.WriteLine("str1: " + str1);
+            Console.WriteLine("Длина строки str1: " + str1.Length);
+
+            strLow = str1.ToLower(CultureInfo.CurrentCulture);
+            strUp = str1.ToUpper(CultureInfo.CurrentCulture);
+            Console.WriteLine("Вариант строки str1, " + 
+                              "набраный строчными буквами: \n"  + strLow);
+            Console.WriteLine("Вариант строки str1, " + 
+                              "набраный прописными буквами: \n"  + strUp);
+            
+            Console.WriteLine();
+            
+            Console.WriteLine("Вывод строки str1 правильно.");
+            for (int i = 0; i < str1.Length; i++)
+                Console.Write(str1[i]);
+            
+            Console.WriteLine();
+
+            if (str1 == str2)
+                Console.WriteLine("str1 == str2");
+            else
+                Console.WriteLine("str1 != str2");
+            
+            if (str1 == str3)
+                Console.WriteLine("str1 == str3");
+            else
+                Console.WriteLine("str1 != str3");
+
+            result = string.Compare(str3, str1, StringComparison.CurrentCulture);
+
+            if (result == 0)
+            {
+                Console.WriteLine("Строки и равны");
+            }
+            else if (result < 0)
+            {
+                Console.WriteLine("Строка меньше строки ");
+            }
+            else Console.WriteLine("Строка больше строки");
+            
+            Console.WriteLine();
+
+            str2 = "Один Два Три Один";
+
+            idx = str2.IndexOf("Один", StringComparison.Ordinal);
+            Console.WriteLine("Индекс первого вхождения подстроки <Один>: " + idx);
+            idx = str2.LastIndexOf("Один", StringComparison.Ordinal);
+            Console.WriteLine("Индекс последнего вхождения подстроки <Один>: " + idx);
+        }
+    }
+}
