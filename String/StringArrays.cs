cf781b9c38af47c1dd8b58aefc28f1dabab55df6
Yevhenii Sirenko <ew_sirenko@ukr.net>
2021-05-16 19:54:15 +0300

    Learn string arrays

---
+++ String/StringArrays.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+
+namespace BasicLearnCSharp
+{
+    class StringArrays
+    {
+        static void Main(string[] args)
+        {
+            string[] str = {"Это", "очень", "простой", "тест."};
+            
+            Console.WriteLine("Исходный масив: ");
+            for (int i = 0; i < str.Length; i++)
+            {
+                Console.Write(str[i] + " ");
+            }
+            Console.WriteLine("\n");
+
+            str[1] = "тоже";
+            str[3] = "до предела тест!";
+            
+            Console.WriteLine("Видоизмененный масив: ");
+            for (int i = 0; i < str.Length; i++)
+            {
+                Console.Write(str[i] + " ");
+            }
+        }
+    }
+}
