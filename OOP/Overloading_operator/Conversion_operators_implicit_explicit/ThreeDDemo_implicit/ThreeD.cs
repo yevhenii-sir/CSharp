4b14f1921d7927929c7e6122911b313a0ee743ef
Yevhenii Sirenko <ew_sirenko@ukr.net>
2021-06-16 18:58:11 +0300

    Learn conversion operator implicit

---
+++ OOP/Overloading_operator/Conversion_operators_implicit_explicit/ThreeDDemo_implicit/ThreeD.cs
@@ -0,0 +1,34 @@
+using System;
+
+namespace BasicLearnCSharp
+{
+    public class ThreeD
+    {
+        private int x, y, z;
+        
+        public ThreeD() : this(0, 0, 0) {}
+
+        public ThreeD(int x, int y, int z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+
+        public static implicit operator int(ThreeD op1)
+        {
+            return op1.x * op1.y * op1.z;
+        }
+        
+        public static ThreeD operator +(ThreeD op1, ThreeD op2)
+        {
+            ThreeD result = new ThreeD {x = op1.x + op2.x, y = op1.y + op2.y, z = op1.z + op2.z};
+
+            return result;
+        }
+        public void Show()
+        {
+            Console.WriteLine(x + ", " + y + ", " + z);
+        }
+    }
+}
---
+++ OOP/Overloading_operator/Conversion_operators_implicit_explicit/ThreeDDemo_implicit/ThreeDDemo.cs
@@ -0,0 +1,38 @@
+using System;
+
+namespace BasicLearnCSharp
+{
+    static class ThreeDDemo
+    {
+        static void Main(string[] args)
+        {
+            ThreeD a = new ThreeD(1, 2, 3);
+            ThreeD b = new ThreeD(10, 10, 10);
+            ThreeD c = new ThreeD();
+            int i;
+            
+            Console.Write("Координаты точки а: ");
+            a.Show();
+
+            Console.Write("Координаты точки b: ");
+            b.Show();
+            
+            Console.Write("Координаты точки c: ");
+            c.Show();
+            
+            Console.WriteLine();
+
+            c = a + b;
+            Console.Write("Резаультат сложения a + b: ");
+            c.Show();
+            Console.WriteLine();
+
+            i = a;
+            Console.WriteLine("Результат присваивания i = a: " + i);
+            Console.WriteLine();
+
+            i = a * 2 - b;
+            Console.WriteLine("Результат вычисления выражения a * 2 - b: " + i);
+        }
+    }
+}
