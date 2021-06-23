722f5876c88b79aeb97bf4719011427837158a5c
Yevhenii Sirenko <ew_sirenko@ukr.net>
2021-06-23 22:54:22 +0300

    Example method call by value

---
+++ OOP/Inheritance/Class_Object/MathOnValue.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace CSharpLearn
+{
+    static class MethOnValue
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(10.ToString());
+        }
+    }
+}
