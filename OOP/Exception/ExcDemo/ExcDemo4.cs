74d0df42fa860cf17eed9fe02f938665bc27aae5
Yevhenii Sirenko <ew_sirenko@ukr.net>
2021-06-27 23:10:02 +0300

    Add example of using multiple catch blocks

---
+++ OOP/Exception/ExcDemo/ExcDemo4.cs
@@ -0,0 +1,32 @@
+using System;
+
+namespace LearnCSharpException
+{
+    class ExcDemo4
+    {
+        static void Main(string[] args)
+        {
+            int[] numer = {4, 8, 16, 32, 64, 128, 256, 512};
+            int[] denom = {2, 0, 4, 4, 0, 8};
+
+            for (int i = 0; i < numer.Length; i++) 
+            {
+                try
+                {
+                    Console.WriteLine(numer[i] + " / " +
+                                      denom[i] + " равно " +
+                                      numer[i] / denom[i]);
+                }
+                catch (DivideByZeroException)
+                {
+                    Console.WriteLine("Делить на нуль нельзя!");
+                }
+                catch (IndexOutOfRangeException)
+                {
+                    Console.WriteLine("Подходящий елемент не найден.");
+                }
+            }
+        }
+    }
+}
+
