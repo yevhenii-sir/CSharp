0225214d512d40451d7d7919c73ad9677b0ee055
Yevhenii Sirenko <ew_sirenko@ukr.net>
2021-07-04 16:12:11 +0300

    Learn directive #else

--- Introduction_to_C#/Begining/Preprocessor/Test1.cs
+++ Introduction_to_C#/Begining/Preprocessor/Test1.cs
@@ -9,12 +9,15 @@ static class Test
     {
 #if EXPERIMENTAL
         Console.WriteLine("Компилируется для эксперементальной версии.");
+#else
+        Console.WriteLine("Компилируется для отдельной версии.");
 #endif
 
 #if EXPERIMENTAL && TRIAL
         Console.WriteLine("Проверка пробной эксперементальной версии.");
+#else
+        Console.WriteLine("Это не пробная експерементальная версия.")
 #endif
         Console.WriteLine("Присутствует во всех версиях.");
-        
     }
 }
