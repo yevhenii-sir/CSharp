1a76443db0aff6e3175bfb729fe26edc2c0d8bfd
Yevhenii Sirenko <ew_sirenko@ukr.net>
2021-07-04 15:51:31 +0300

    Example of using an identifier expression

--- Introduction_to_C#/Begining/Preprocessor/Test1.cs
+++ Introduction_to_C#/Begining/Preprocessor/Test1.cs
@@ -1,4 +1,5 @@
 #define EXPERIMENTAL
+#define TRIAL
 
 using System;
 
@@ -8,6 +9,10 @@ static class Test
     {
 #if EXPERIMENTAL
         Console.WriteLine("Компилируется для эксперементальной версии.");
+#endif
+
+#if EXPERIMENTAL && TRIAL
+        Console.WriteLine("Проверка пробной эксперементальной версии.");
 #endif
         Console.WriteLine("Присутствует во всех версиях.");
         
