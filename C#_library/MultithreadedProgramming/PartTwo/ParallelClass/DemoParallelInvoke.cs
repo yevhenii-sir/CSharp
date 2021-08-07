305c28e49ed0479d95fe996dcdddbc40c5e79d9c
Yevhenii Sirenko <ew_sirenko@ukr.net>
2021-08-07 15:02:19 +0300

    Using lambda expressions instead of named methods

--- C#_library/MultithreadedProgramming/PartTwo/ParallelClass/DemoParallelInvoke.cs
+++ C#_library/MultithreadedProgramming/PartTwo/ParallelClass/DemoParallelInvoke.cs
@@ -6,37 +6,34 @@ namespace LearnCSharpMultiThrProgramming
 {
     static class DemoParallel
     {
-        static void MyMeth()
-        {
-            Console.WriteLine("MyMeth запущен");
-
-            for (int count = 0; count < 5; count++)
-            {
-                Thread.Sleep(500);
-                Console.WriteLine("В методе MyMeth() подсчет равен " + count);
-            }
-
-            Console.WriteLine("MyMeth() завершен");
-        }
-        
-        static void MyMeth2()
-        {
-            Console.WriteLine("MyMeth2() запущен");
-
-            for (int count = 0; count < 5; count++)
-            {
-                Thread.Sleep(500);
-                Console.WriteLine("В методе MyMeth2() подсчет равен " + count);
-            }
-
-            Console.WriteLine("MyMeth2() завершен");
-        }
-
         static void Main(string[] args)
         {
             Console.WriteLine("Основной поток запущен.");
             
-            Parallel.Invoke(MyMeth, MyMeth2);
+            Parallel.Invoke(() =>
+            {
+                Console.WriteLine("MyMeth запущен");
+
+                for (int count = 0; count < 5; count++)
+                {
+                    Thread.Sleep(500);
+                    Console.WriteLine("В методе MyMeth() подсчет равен " + count);
+                }
+
+                Console.WriteLine("MyMeth() завершен");
+            }, 
+                () =>
+            {
+                Console.WriteLine("MyMeth2() запущен");
+
+                for (int count = 0; count < 5; count++)
+                {
+                    Thread.Sleep(500);
+                    Console.WriteLine("В методе MyMeth2() подсчет равен " + count);
+                }
+
+                Console.WriteLine("MyMeth2() завершен");
+            });
             
             Console.WriteLine("Основной поток завершен.");
         }
