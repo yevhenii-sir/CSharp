4da08d11ae53edc1269b2203e1e347a288e500cc
Yevhenii Sirenko <ew_sirenko@ukr.net>
2021-08-07 22:45:29 +0300

    Added an example to display the running time of parallel computation and sequential

--- C#_library/MultithreadedProgramming/PartTwo/ParallelClass/DemoParallelFor.cs
+++ C#_library/MultithreadedProgramming/PartTwo/ParallelClass/DemoParallelFor.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Diagnostics;
 using System.Threading.Tasks;
 
 namespace LearnCSharpMultiThrProgramming
@@ -21,12 +22,42 @@ namespace LearnCSharpMultiThrProgramming
             Console.WriteLine("Основной поток запущен.");
 
             _data = new int[100_000_000];
-
+            
+            Stopwatch sw = Stopwatch.StartNew();
+            Parallel.For(0, _data.Length, (i) => _data[i] = i);
+            sw.Stop();
+            Console.WriteLine("Параллельно выполняемый цикл инициализации: {0} секунд.", sw.Elapsed.TotalSeconds);
+            sw.Reset();
+            
+            sw.Start();
             for (int i = 0; i < _data.Length; i++) _data[i] = i;
+            sw.Stop();
+            Console.WriteLine("Последовательно выполняемый цикл инициализации: {0} секунд.", sw.Elapsed.TotalSeconds);
+            sw.Reset();
+            Console.WriteLine();
 
+            sw.Start();
             Parallel.For(0, _data.Length, MyTransform);
+            sw.Stop();
+            Console.WriteLine("Параллельно выполняемый цикл преобразования: {0} секунд.", sw.Elapsed.TotalSeconds);
+            sw.Reset();
+
+            sw.Start();
+            for (int i = 0; i < _data.Length; i++) MyTransform(i);
+            sw.Stop();
+            Console.WriteLine("Последовательно выполняемый цикл преобразования: {0} секунд.", sw.Elapsed.TotalSeconds);
 
             Console.WriteLine("Основной поток завершен.");
         }
     }
 }
+
+/*
+Основной поток запущен.
+Параллельно выполняемый цикл инициализации: 0.1505218 секунд.
+Последовательно выполняемый цикл инициализации: 0.2017253 секунд.
+
+Параллельно выполняемый цикл преобразования: 0.4245728 секунд.
+Последовательно выполняемый цикл преобразования: 1.1605602 секунд.
+Основной поток завершен.
+*/
