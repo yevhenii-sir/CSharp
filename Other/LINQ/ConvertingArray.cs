7042521e7460a9a2f4fedffe62e0ded940b72fad
Yevhenii Sirenko <ew_sirenko@ukr.net>
2021-08-24 23:21:54 +0300

    Comparison of using LINQ to convert an array to one-dimensional versus using foreach and List

--- Other/LINQ/ConvertingArray.cs
+++ Other/LINQ/ConvertingArray.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 
 namespace LearnCSharpOther
@@ -13,16 +15,33 @@ namespace LearnCSharpOther
                 new int[] { 4, 5 }, 
                 new int[] { 6, 7, 8, 9 }
             };
-
+            Stopwatch stopwatch = Stopwatch.StartNew();
+            
             var array = inter.SelectMany(inner => inner.Select(innerInner => innerInner));
             
+            stopwatch.Stop();
+            Console.WriteLine("Использование LINQ: " + stopwatch.Elapsed.TotalMilliseconds + " мс");
+            stopwatch.Restart();
+            
+            List<int> array2 = new List<int>();
+            foreach (var t in inter)
+                foreach (var t1 in t)
+                    array2.Add(t1);
+            
+            stopwatch.Stop();
+            Console.WriteLine("Использование foreach и List: " + stopwatch.Elapsed.TotalMilliseconds + " мс");
+
             Console.WriteLine(String.Join(" ", array));
+            Console.WriteLine(String.Join(" ", array2));
         }
     }
 }
 
 /*
 
+Использование LINQ: 3.153 мс
+Использование foreach и List: 0.0822 мс
+1 2 3 4 5 6 7 8 9
 1 2 3 4 5 6 7 8 9
 
 */
